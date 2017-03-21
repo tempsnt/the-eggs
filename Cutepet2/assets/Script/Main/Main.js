a2382ebddf46855cebe08386c027958d3c9366ac
tempsnt <temps0@163.com>
2017-03-21 15:56:51 +0800

    进入开发分支 开始开发

--- Cutepet2/assets/Script/Main/Main.js
+++ Cutepet2/assets/Script/Main/Main.js
@@ -13,15 +13,24 @@ cc.Class({ //测试版本管理GIT 这是版本2
     var scene = cc.director.getScene();
     var node = cc.instantiate(this.target);
 
- var label = this.node.getChildByName("BackGround");
+ var BG = this.node.getChildByName("BackGround");
  //node.parent = scene;
   //console.log(label);
-    node.parent = label;
+    node.parent = BG;
     node.setPosition(0,0);
+   
+    
+    node.scaleX =0.85;
+    node.scaleY =0.85;
+    //var w1 = node.getComponent(cc.Widget);
+   // var w1 = node.getComponentsInChildren(cc.Widget);
+    //console.log("Widget"+w1);
+//    w1.top=10;w1.left=50;w1.right=0;
+    //w1. isAlignHorizontalCenter=true;
+ //   w1. horizontalCenter=0;
     console.log(node.position.x+"-"+node.position.y);
     
-//    var MenuBar = label.getChildByName("MenuBar");
-//    MenuBar.setPosition(0,-200);
+
 
     },
     onLoad: function () {
